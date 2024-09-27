05444b5161e4a5d303329b9b64afe61480e37876
Leon Kountouras <koundouras@gmail.com>
2024-09-27 16:38:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,23 +13,23 @@ for (let i = 0; i < gridlength; i++) {
     const group = Math.floor(i / 10);
     if (group % 2 === 1) { // If the group is odd
       if (i % 2 === 1) { // If the index is odd
-        console.log("if the group is odd and index is odd" , i)
+        console.log(`if the group ${group} is odd and index ${i} is odd`)
         div.classList.add('blue'); // Add the "blue" class
        // div.classList.remove('white', 'stripe'); // Remove the "white" and "stripe" classes
       } else { // If the index is even
-        console.log("if the group is odd and index is even" , i)
+        console.log(`if the group ${group} is odd and index ${i} is even`)
         div.classList.add('stripe'); // Add the "stripe" class
         //div.classList.remove('blue', 'white'); // Remove the "blue" and "white" classes
       }
     }  
     else if (group % 2 === 0) { // If the group is even
         if (i % 2 === 1) { // If the index is odd
-          console.log("if the group is even and index is odd" , i)
+          console.log(`if the group ${group} is even and index ${i} is odd`)
           div.classList.add('stripe'); // Add the "stripe" class
           //div.classList.remove('blue', 'white'); // Remove the "blue" and "white" classes
       } else { // If the index is even
           div.classList.add('white'); // Add the "white" class
-          console.log("if the group is even and index is even" , i)
+          console.log(`if the group ${group} is even and index ${i} is even`)
          // div.classList.remove('blue', 'stripe'); // Remove the "blue" and "stripe" classes
       }
     } 
