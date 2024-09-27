120b833336c61c510e5e1935a475b50ce65bcec0
Leon Kountouras <koundouras@gmail.com>
2024-09-27 16:23:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,16 +11,7 @@ for (let i = 0; i < gridlength; i++) {
     div.classList.add("square");
     
     const group = Math.floor(i / 10);
-    
-if (group % 2 === 0) { // If the group is even
-  if (i % 2 === 1) { // If the index is odd
-    div.classList.add('blue'); // Add the "blue" class
-    div.classList.remove('white', 'stripe'); // Remove the "white" and "stripe" classes
-  } else { // If the index is even
-    div.classList.add('stripe'); // Add the "stripe" class
-    div.classList.remove('blue', 'white'); // Remove the "blue" and "white" classes
-  }
-} else { // If the group is odd
+  if (group % 2 === 1) { // If the group is odd
   if (i % 2 === 1) { // If the index is odd
     div.classList.add('stripe'); // Add the "stripe" class
     div.classList.remove('blue', 'white'); // Remove the "blue" and "white" classes
@@ -28,7 +19,18 @@ if (group % 2 === 0) { // If the group is even
     div.classList.add('white'); // Add the "white" class
     div.classList.remove('blue', 'stripe'); // Remove the "blue" and "stripe" classes
   }
-}
+  }  
+    
+else if (group % 2 === 0) { // If the group is even
+  if (i % 2 === 1) { // If the index is odd
+    div.classList.add('blue'); // Add the "blue" class
+    div.classList.remove('white', 'stripe'); // Remove the "white" and "stripe" classes
+  } else { // If the index is even
+    div.classList.add('stripe'); // Add the "stripe" class
+    div.classList.remove('blue', 'white'); // Remove the "blue" and "white" classes
+  }
+} 
+
     
     player1Board.appendChild(div);
     player2Board.appendChild(div.cloneNode(true));
