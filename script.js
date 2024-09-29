cd1a93e9bf02d30c8966440acdda972fcf16f942
Leon Kountouras <koundouras@gmail.com>
2024-09-29 15:51:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const player1Board = document.getElementById('player1-board');
 const player2Board = document.getElementById('player2-board');
 // Create a 10x10 grid for each player
 let gridlength = 10;
-for (let i = 0; i < gridlength; i++) {
+for (let i = 1; i <= gridlength; i++) {
   console.log(`i : ${i}`)
  // const group = Math.floor(i / 10);
   const group = i;
