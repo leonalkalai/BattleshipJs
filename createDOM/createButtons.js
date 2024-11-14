bb4cd376d327934d3e8ee13d5cde7b2b995cb6c3
Leon Koundouras <Koundouras@hotmail.com>
2024-11-14 19:43:42 +0200

    fix missing event toggleShipRotation

--- createDOM/createButtons.js
+++ createDOM/createButtons.js
@@ -20,7 +20,7 @@ export function createButtons(mainElement) {
       }
       if (buttonName === "Rotate") {
         button.classList.add("ready");
-        button.addEventListener("click", );
+        button.addEventListener("click", toggleShipRotation);
         button.addEventListener("touchstart", () => toggleShipRotation());
       }
       button.id = `${buttonName.toLowerCase()}${buttonPrefix}`;
