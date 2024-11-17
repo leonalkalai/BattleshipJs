f51d0e70b7f67aa8762e68d17e3969211667feab
Leon Koundouras <Koundouras@hotmail.com>
2024-11-17 14:46:56 +0200

    Updates

--- js/renderDOM/gameLogic/playSoundEffects.js
+++ js/renderDOM/gameLogic/playSoundEffects.js
@@ -1,1 +1,1 @@
-function n(o){const e=new Audio(`../assets/sounds/${o}.mp3`);e.loop=!1,e.type="audio/mpeg",e.addEventListener("canplaythrough",a=>{e.play()})}export{n as p};
+function n(o){const e=new Audio(`../../../../assets/sounds/${o}.mp3`);e.loop=!1,e.type="audio/mpeg",e.addEventListener("canplaythrough",a=>{e.play()})}export{n as p};
