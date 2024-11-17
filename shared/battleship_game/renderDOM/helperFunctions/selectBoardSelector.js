58e252e5a80214f0f472868a7a030ab58ef8b770
Leon Koundouras <Koundouras@hotmail.com>
2024-11-17 13:21:02 +0200

    Updates

--- shared/battleship_game/renderDOM/helperFunctions/selectBoardSelector.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["shared/battleship_game/renderDOM/helperFunctions/helperFunctionsClass.js","fallback.js"])))=>i.map(i=>d[i]);
-import{_ as a}from"../../../../fallback.js";const{helperFunctionsClass:o}=await a(async()=>{const{helperFunctionsClass:t}=await import("./helperFunctionsClass.js");return{helperFunctionsClass:t}},__vite__mapDeps([0,1])),{gameState:r}=await a(async()=>{const{gameState:t}=await import("../gameLogic/GameState.js");return{gameState:t}},[]);
