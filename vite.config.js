32ba922ba2aa327a3c00d1e9968cc69fbedccbf3
Leon Koundouras <Koundouras@hotmail.com>
2024-11-20 11:11:43 +0200

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,70 @@
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { defineConfig } from "vite";
 import path from "path";
 
-const svgoPlugins = [
-  { name: 'removeDoctype', active: true },
-  { name: 'removeComments', active: true },
-  { name: 'removeMetadata', active: true },
-  { name: 'removeTitle', active: true },
-  { name: 'removeDesc', active: true },
-  { name: 'removeDimensions', active: true },
-  { name: 'convertColors', params: { currentColor: true } },
-  { name: 'removeUnusedNS', active: true },
-  { name: 'cleanupIDs', params: { remove: true } },
-  { name: 'mergePaths', active: true },
-  { name: 'convertShapeToPath', active: true },
-];
-
+const DEFAULT_OPTIONS = {
+  test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+  exclude: undefined,
+  include: undefined,
+  includePublic: true,
+  logStats: true,
+  ansiColors: true,
+  svg: {
+    multipass: true,
+    plugins: [
+      {
+        name: "preset-default",
+        params: {
+          overrides: {
+            cleanupNumericValues: false,
+            removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+          },
+          cleanupIDs: {
+            minify: false,
+            remove: false,
+          },
+          convertPathData: false,
+        },
+      },
+      "sortAttrs",
+      {
+        name: "addAttributesToSVGElement",
+        params: {
+          attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+        },
+      },
+    ],
+  },
+  png: {
+    // https://sharp.pixelplumbing.com/api-output#png
+    quality: 100,
+  },
+  jpeg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 100,
+  },
+  jpg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 100,
+  },
+  tiff: {
+    // https://sharp.pixelplumbing.com/api-output#tiff
+    quality: 100,
+  },
+  // gif does not support lossless compression
+  // https://sharp.pixelplumbing.com/api-output#gif
+  gif: {},
+  webp: {
+    // https://sharp.pixelplumbing.com/api-output#webp
+    lossless: true,
+  },
+  avif: {
+    // https://sharp.pixelplumbing.com/api-output#avif
+    lossless: true,
+  },
+  cache: false,
+  cacheLocation: undefined,
+};
 
 export default defineConfig({
   root: "./", // Set the root to the current directory (project root)
@@ -29,23 +78,32 @@ export default defineConfig({
         main: "./src/main.js",
         // createDOM files
         createBoard: "./src/js/createDOM/createBoard/createBoard.js",
-        createBoardPattern: "./src/js/createDOM/createBoard/createBoardPattern.js",
-        createBoardTitles: "./src/js/createDOM/createBoard/createBoardTitles.js",
+        createBoardPattern:
+          "./src/js/createDOM/createBoard/createBoardPattern.js",
+        createBoardTitles:
+          "./src/js/createDOM/createBoard/createBoardTitles.js",
         createButtons: "./src/js/createDOM/createButtons.js",
         createContainer: "./src/js/createDOM/createContainer.js",
         createGame: "./src/js/createDOM/createGame.js",
         createHeader: "./src/js/createDOM/createHeader.js",
         createMain: "./src/js/createDOM/createMain.js",
         // renderDOM files (game logic, helper functions, init)
-        canPlaceShip: "./src/js/renderDOM/gameLogic/handlePlacement/canPlaceShip.js",
-        handleShipPlacement: "./src/js/renderDOM/gameLogic/handlePlacement/handleShipPlacement.js",
-        placeEnemyShips: "./src/js/renderDOM/gameLogic/handlePlacement/placeEnemyShips.js",
-        placeHumanShips: "./src/js/renderDOM/gameLogic/handlePlacement/placeHumanShips.js",
+        canPlaceShip:
+          "./src/js/renderDOM/gameLogic/handlePlacement/canPlaceShip.js",
+        handleShipPlacement:
+          "./src/js/renderDOM/gameLogic/handlePlacement/handleShipPlacement.js",
+        placeEnemyShips:
+          "./src/js/renderDOM/gameLogic/handlePlacement/placeEnemyShips.js",
+        placeHumanShips:
+          "./src/js/renderDOM/gameLogic/handlePlacement/placeHumanShips.js",
         placeShip: "./src/js/renderDOM/gameLogic/handlePlacement/placeShip.js",
-        preventClickHumanBoard: "./src/js/renderDOM/gameLogic/handlePlacement/preventClickHumanBoard.js",
-        toggleShipRotation: "./src/js/renderDOM/gameLogic/handlePlacement/toggleShipRotation.js",
+        preventClickHumanBoard:
+          "./src/js/renderDOM/gameLogic/handlePlacement/preventClickHumanBoard.js",
+        toggleShipRotation:
+          "./src/js/renderDOM/gameLogic/handlePlacement/toggleShipRotation.js",
         attack: "./src/js/renderDOM/gameLogic/attack.js",
-        checkShipDestroyed: "./src/js/renderDOM/gameLogic/checkShipDestroyed.js",
+        checkShipDestroyed:
+          "./src/js/renderDOM/gameLogic/checkShipDestroyed.js",
         checkWin: "./src/js/renderDOM/gameLogic/checkWin.js",
         enemyTurn: "./src/js/renderDOM/gameLogic/enemyTurn.js",
         GameState: "./src/js/renderDOM/gameLogic/GameState.js",
@@ -53,9 +111,11 @@ export default defineConfig({
         restartGame: "./src/js/renderDOM/gameLogic/restartGame.js",
         startGame: "./src/js/renderDOM/gameLogic/startGame.js",
         updateShipStatus: "./src/js/renderDOM/gameLogic/updateShipStatus.js",
-        updateTurnIndicator: "./src/js/renderDOM/gameLogic/updateTurnIndicator.js",
+        updateTurnIndicator:
+          "./src/js/renderDOM/gameLogic/updateTurnIndicator.js",
         // helper functions
-        helperFunctionsClass: "./src/js/renderDOM/helperFunctions/helperFunctionsClass.js",
+        helperFunctionsClass:
+          "./src/js/renderDOM/helperFunctions/helperFunctionsClass.js",
         // initGame
         init: "./src/js/renderDOM/initGame/init.js",
         initGame: "./src/js/renderDOM/initGame/initGame.js",
@@ -63,24 +123,33 @@ export default defineConfig({
       output: {
         entryFileNames: (chunkInfo) => {
           if (chunkInfo && chunkInfo.facadeModuleId) {
-            const path = chunkInfo.facadeModuleId.split('/').slice(1).join('/').replace("shared/battleship_game/src/", "");
-            
-             // Prevent .html from being processed as a JS chunk
-             if (path.endsWith('.html')) {
+            const path = chunkInfo.facadeModuleId
+              .split("/")
+              .slice(1)
+              .join("/")
+              .replace("shared/battleship_game/src/", "");
 
-              return path.replace("shared/battleship_game/", "").replace(".html", ".js"); // Ensure HTML files are not processed here
+            // Prevent .html from being processed as a JS chunk
+            if (path.endsWith(".html")) {
+              return path
+                .replace("shared/battleship_game/", "")
+                .replace(".html", ".js"); // Ensure HTML files are not processed here
             }
 
             return `${path}`;
           }
-          return 'fallback.js'; // Fallback if the path is not found
+          return "fallback.js"; // Fallback if the path is not found
         },
         chunkFileNames: (chunkInfo) => {
           if (chunkInfo && chunkInfo.facadeModuleId) {
-            const path = chunkInfo.facadeModuleId.split('/').slice(1).join('/').replace("shared/battleship_game/src/", "");
+            const path = chunkInfo.facadeModuleId
+              .split("/")
+              .slice(1)
+              .join("/")
+              .replace("shared/battleship_game/src/", "");
             return `${path}`;
           }
-          return 'fallback.js'; // Fallback if the path is not found
+          return "fallback.js"; // Fallback if the path is not found
         },
         // assetFileNames: ({ names = [] }) => {
         //   const [name = ''] = names;
@@ -91,18 +160,17 @@ export default defineConfig({
         //   }else if (/\.(mp3)$/i.test(name)) {
         //     return 'assets/sounds/[name][extname]';
         //   }
-          
+
         //   return '[name]-[hash][extname]';
         // },
         assetFileNames: (assetInfo) => {
           if (!assetInfo || !assetInfo.name) {
-            return 'assets/fallback'; // Fallback if the asset name is missing
+            return "assets/fallback"; // Fallback if the asset name is missing
           }
 
-          const extType = assetInfo.name.split('.').pop(); // Get file extension
-          
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+          const extType = assetInfo.name.split(".").pop(); // Get file extension
 
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
             return `assets/images/${assetInfo.name}`;
           }
           if (/mp3/i.test(extType)) {
@@ -118,15 +186,16 @@ export default defineConfig({
   },
   base: "/BattleshipJs/", // Base path for GitHub Pages deployment
   plugins: [
-    imagemin({
-      optipng: {
-        optimizationLevel: 7, // Optimize PNGs with maximum compression
-      },
-      mozjpeg: {
-        quality: 75, // Set JPEG quality to 75
-      },
-      svgo: {
-        plugins:svgoPlugins,
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 75 },
+      webp: { quality: 80 },
+      avif: { quality: 70 },
+      svg: {
+        plugins: [
+          { name: "removeViewBox", active: false },
+          { name: "sortAttrs" },
+        ],
       },
     }),
   ],
